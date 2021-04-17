42135569d535c1f0a4ad9b611ee7218dc547bb21
Султан <sultan-avto5006@mail.ru>
2021-04-17 03:04:51 +0300

    Read data from simulink model

--- IKinemTh.m
+++ IKinemTh.m
@@ -1,10 +1,10 @@
 function [theta] = IKinemTh(x0, y0, z0,L)
-
+    
     R = L.Base_Length; % радиус окружности основания дельта-робота
     r = L.EndEffector_Length; % радиус окружности выходного звена дельта-робота
     l=L.FirstLinc ; % длина первого сочленения
     L1 = L.SecondLinc;% длина второго сочленения
-
+    
     y1 = -R;
     y0 = y0-r; % shift center to edg    
     a = (x0^2 + y0^2 + z0^2 + L1^2 - l^2 - y1^2)/(2*z0);
